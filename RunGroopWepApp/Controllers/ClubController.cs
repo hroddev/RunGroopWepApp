ae7bd0c412f77bc1ca67df13ebcacd32565f076a
Hector Rodriguez R <hector@hrod.dev>
2023-08-06 17:29:18 -0500

    Feat: Update the Views with data at the Club and Race

--- RunGroopWepApp/Controllers/ClubController.cs
+++ RunGroopWepApp/Controllers/ClubController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RunGroopWepApp.Data;
+using RunGroopWepApp.Models;
 
 namespace RunGroopWepApp.Controllers
 {
     public class ClubController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ClubController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Club> clubs = _context.Clubs.ToList();
+            return View(clubs);
         }
     }
 }
--- RunGroopWepApp/Controllers/RaceController.cs
+++ RunGroopWepApp/Controllers/RaceController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RunGroopWepApp.Data;
+using RunGroopWepApp.Models;
 
 namespace RunGroopWepApp.Controllers
 {
+   
     public class RaceController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public RaceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Race> races = _context.Races.ToList();
+            return View(races);
         }
     }
 }
