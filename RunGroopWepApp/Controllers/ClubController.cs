76b9e0d5f13b905d48ae4009e8fbb571ae74aa10
Hector Rodriguez R <hector@hrod.dev>
2023-08-06 15:30:04 -0500

    Feat: Create the template of Club and Race controller and view

---
+++ RunGroopWepApp/Controllers/ClubController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RunGroopWepApp.Controllers
+{
+    public class ClubController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
