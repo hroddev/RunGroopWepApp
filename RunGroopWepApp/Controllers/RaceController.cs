1dbeb8cfa217dc0e84e5bc279b00410347683353
Hector Rodriguez R <hector@hrod.dev>
2023-08-06 20:29:20 -0500

    Feat: Create Detail Views of Club and Race pages

--- RunGroopWepApp/Controllers/RaceController.cs
+++ RunGroopWepApp/Controllers/RaceController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RunGroopWepApp.Data;
 using RunGroopWepApp.Models;
 
@@ -17,6 +18,12 @@ namespace RunGroopWepApp.Controllers
         {
             List<Race> races = _context.Races.ToList();
             return View(races);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            Race race = _context.Races.Include(a => a.Address).FirstOrDefault(r => r.Id == id);
+            return View(race);
         }
     }
 }
